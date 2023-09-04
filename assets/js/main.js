fa2eeadb63d22ffffdd6c81d8b9ebd0b6fe043db
ctrl-alter-ego <adyharold+github@gmail.com>
2023-09-04 18:49:14 +0100

    Adds basic privacy and cookies pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,62 +92,7 @@
   };
 })();
 
-// ======== animate heading - changing word
-var changingWords = ['company', 'innovation', 'product', 'project', 'ideas'];
-var changingWordElement = document.getElementById('changing-word');
-var cursorElement = document.querySelector('.cursor');
-var _PART = 0;
-var _PART_INDEX = 0;
-var _INTERVAL_VAL;
 
-
-function Type() { 
-	var text =  changingWords[_PART].substring(0, _PART_INDEX + 1);
-	changingWordElement.innerHTML = text;
-	_PART_INDEX++;
-
-	// If full sentence has been displayed then start to delete the sentence after some time
-	if(text === changingWords[_PART]) {
-		// Hide the cursor
-	//	cursorElement.style.display = 'none';
-
-		clearInterval(_INTERVAL_VAL);
-		setTimeout(function() {
-			_INTERVAL_VAL = setInterval(Delete, 50);
-		}, 1000);
-	}
-}
-
-// Implements deleting effect
-function Delete() {
-	// Get substring with 1 characater deleted
-	var text =  changingWords[_PART].substring(0, _PART_INDEX - 1);
-	changingWordElement.innerHTML = text;
-	_PART_INDEX--;
-
-	// If sentence has been deleted then start to display the next sentence
-	if(text === '') {
-		clearInterval(_INTERVAL_VAL);
-
-		// If current sentence was last then display the first one, else move to the next
-		if(_PART == (changingWords.length - 1))
-			_PART = 0;
-		else
-			_PART++;
-		
-		_PART_INDEX = 0;
-
-		// Start to display the next sentence after some time
-		setTimeout(function() {
-			cursorElement.style.display = 'inline-block';
-			_INTERVAL_VAL = setInterval(Type, 100);
-		}, 500);
-	}
-}
-
-setTimeout(() => {
-  _INTERVAL_VAL = setInterval(Type, 100);
-}, 2000);
 
 // ========== flash input box
 
